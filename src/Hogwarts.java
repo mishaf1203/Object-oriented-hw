175dded47687d24becb635e518bf213cc0dde768
mishaf1203 <mishashaferoff@gmail.com>
2022-08-01 22:34:57 +0300

    3 commit

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -9,5 +9,4 @@ public class Hogwarts {
         this.apparition = apparition;
     }
 
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gryffindor[] gryffindors = {
+                new Gryffindor(7,
+                        7,
+                        10,
+                        "Harry Potter",
+                        8,
+                        6),
+                new Gryffindor(5,
+                        7,
+                        5,
+                        "Ron Weasley",
+                        5,
+                        4),
+
+                new Gryffindor(8,
+                        7,
+                        7,
+                        "Hermione Granger",
+                        8,
+                        10)
+        };
+        Slytherin[] slytherins = {
+                new Slytherin("Drako Malfoy",
+                        7, 10, 7, 8, 6,
+                        9),
+                new Slytherin("Graham Montegrue", 7, 5, 3, 5, 4, 3),
+
+                new Slytherin("Gregory Goil",
+                        7,
+                        7,
+                        5,
+                        8,
+                        10, 4)
+        };
+        Ravenclaw[] ravenclaws = {
+                new Ravenclaw("Drako Malfoy",
+                        7,
+                        10,
+                        7,
+                        8,
+                        6),
+
+                new Ravenclaw("Graham Montegrue",
+                        7,
+                        5,
+                        3,
+                        5,
+                        4),
+
+                new Ravenclaw("Gregory Goil",
+                        7,
+                        7,
+                        5,
+                        8,
+                        10)
+        };
+        Hufflepuff[] hufflepuffs = {
+                new Hufflepuff("Zakharia Smith",
+                        7, 10, 7, 8, 6),
+
+                new Hufflepuff("Sedrick Diggori",
+                        7, 5, 3, 5, 4),
+
+                new Hufflepuff("Justin Finch-Fletchli",
+                        7, 7, 5, 8, 10)
+        };
     }
 }
