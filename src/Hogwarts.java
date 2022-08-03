51ec24d094ad352c10c75e4fea51016f7e255359
mishaf1203 <mishashaferoff@gmail.com>
2022-08-03 13:51:55 +0300

    6 commit

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 
 public class Hogwarts {
-    public String name;
-    public int magic;
-    public int apparition;
+    private String name;
+    private int magic;
+    private int apparition;
 
     public Hogwarts(String name, int magic, int apparition) {
         this.name = name;
